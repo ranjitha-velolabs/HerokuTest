f931176690774d860aa27f82639d5187b1d5fc11
Your Name <you@example.com>
2016-05-01 00:40:44 +0530

    Revert Port

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
     appName: 'skyfleet',
-    port: process.env.SKYFLEET_PORT || '3000',
+    port: process.env.SKYFLEET_PORT || '19754',
     //mapBoxToken: process.env['MAPBOX_TOKEN'] || 'pk.eyJ1IjoiYXJhdmluZDc0MSIsImEiOiJjaWtkbnFlaWUwMDFzdWtseXZtNWNtNjB1In0.xbZ6Ok2qvsL3BNwS1eAEwg'
     mapBoxToken: process.env['MAPBOX_TOKEN'] || 'pk.eyJ1IjoiYWNncmVlbjEyMjYiLCJhIjoiOGNiOGJlZTUyZDI1NDkzODhlNmJhMGVmZDU4OTZiMDcifQ.-MtqKH9lyJ2J1JDsxdmDRQ',
     numberOfPoints: 30000,
