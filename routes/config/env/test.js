147725c903ef01f757d5b649643ace7f5eabae3e
Your Name <you@example.com>
2016-05-01 00:14:34 +0530

    DB Config Production

--- routes/config/env/test.js
+++ routes/config/env/test.js
@@ -2,7 +2,10 @@
 
 //MongoDB configuration for testing environment
 module.exports = {
-  db: 'mongodb://localhost/meanpassportauth_test',
-  redisHost: 'localhost',
-  redisPort:6379
+   db: process.env.DATABASE_URL || 'postgres://vnlbplwzgfvkxg:cDhh2llYeL1fLKMIqwa3h48wMf@ec2-54-204-41-70.compute-1.amazonaws.com:5432/d9rpl071a450n6'
 };
+// module.exports = {
+//   db: 'mongodb://localhost/meanpassportauth_test',
+//   redisHost: 'localhost',
+//   redisPort:6379
+// };
